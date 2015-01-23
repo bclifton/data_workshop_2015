64902cb154948ad6f2a52a3f07828e96868d39bf
Brian Clifton <briancliftonstudio@gmail.com>
2015-01-23 14:51:27 -0500

    Updating repos.

--- Unconference_1/Day 1/js/main.js
+++ Unconference_1/Day 1/js/main.js
@@ -1,1 +1,38 @@
+var KEY = "fcdTp4HyqXQ4QnrwpXa08F7HwUwvOTKqlXwPEGU5I3xuPcOS4vAqA";
+var API_ADDRESS = "https://api.enigma.io/v2/data/" + KEY + "/us.gov.cdc.food?page=";
+
+
+$(document).ready(function(){
+	getData();
+});
+
+function getData() {
+	
+	$.get(API_ADDRESS + 1).done(function(data) {
+		// console.log(data);
+		processData(data.result);
+	});
+
+}
+
+function processData(data) {
+
+	var newData = {};
+	_.each(data, function(horseshitum) {
+		// console.log(horseshit);
+		var year = horseshitum.year;
+		var month = horseshitum.month;
+		var date = year + "-" + month;
+
+		if (_.has(newData, date)) {
+			newData[date] += +horseshitum.totalill;
+		} else {
+			newData[date] = +horseshitum.totalill;
+			// console.log(horseshit.month);
+		}
+	});
+	console.log(newData);
+
+}
+
 
